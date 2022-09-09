edbc79b717067916d2d76be7e123ae5e12e4aaf6
kitkat1900 <moco1900@gmail.com>
2022-09-10 01:11:53 +0900

    articleモデル、テーブル作成

---
+++ db/migrate/20220909160821_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :article_name, null: false
+      t.text :text, null: false
+      t.integer :category_id, null: false
+      t.integer :status, null: false, default: 0
+      t.timestamps
+    end
+  end
+end
