f2851ac05bf086adffce9787b1f6f6c8fc5b049f
kitkat1900 <moco1900@gmail.com>
2022-09-19 02:44:50 +0900

    ユーザー情報変更のルーティング

--- app/javascript/preview _user.js
+++ app/javascript/preview _user.js
@@ -1,29 +1,75 @@
-$(function() {
-  function readURL(input) {
-      if (input.files && input.files[0]) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-  $('#img_prev').attr('src', e.target.result);
+document.addEventListener('DOMContentLoaded', function(){
+  // 新規投稿・編集ページのフォームを取得
+  const postForm = document.getElementById('user-image');
+  // プレビューを表示するためのスペースを取得
+  const previewList = document.getElementById('prev-image');
+  const defaultList = document.getElementById('default-image');
+  const userList = document.getElementById('user-img');
+ 
+  // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
+  if (!postForm) return null;
+
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="user[image]"]');
+  
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', function(e){
+    // 古いプレビューが存在する場合は削除
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
+    const defaultImage = document.querySelector('.default');
+    if (defaultImage) {
+      defaultImage.remove();
+    }
+    const defaultPreview = document.querySelector('#default-image');
+    if (defaultPreview) {
+      defaultPreview.remove();
+    }
+    const userImage = document.querySelector('#user-img');
+    if (userImage) {
+      userImage.remove();
+    }
+    const file = e.target.files[0];
+    if (file) {
+      const blob = window.URL.createObjectURL(file);
+      
+      // 画像を表示するためのdiv要素を生成
+      const previewWrapper = document.createElement('div');
+      previewWrapper.setAttribute('class', 'preview');
+      // 表示する画像を生成
+      const previewImage = document.createElement('img');
+      previewImage.setAttribute('class', 'preview-image');
+      previewImage.setAttribute('src', blob);
+    
+      // 生成したHTMLの要素をブラウザに表示させる
+      previewWrapper.appendChild(previewImage);
+      previewList.appendChild(previewWrapper);
+    } else {
+      if (userList) {
+        const userWrapper = document.createElement('div');
+        userWrapper.setAttribute('class', 'user');
+        const userImage = document.createElement('img')
+        userImage.setAttribute('class', 'user-image');
+        userImage.setAttribute('src', userList.src);
+        userWrapper.appendChild(userImage);
+        previewList.appendChild(userWrapper);
+        console.log(userList.src);
+        console.log(userWrapper);
+        console.log(userImage);
+      } else {
+        const defaultWrapper = document.createElement('div');
+        defaultWrapper.setAttribute('class', 'default');
+        const defaultImage = document.createElement('img')
+        defaultImage.setAttribute('class', 'default-image');
+        defaultImage.setAttribute('src', 'http://localhost:3000/assets/default-a7c52a401515c13bceac48e8883802f8e32627a7d86ca713df73d6ebb7d03412.png');
+        defaultWrapper.appendChild(defaultImage);
+        previewList.appendChild(defaultWrapper);
       }
-      reader.readAsDataURL(input.files[0]);
-      }
-  }
-  $("#user_img").change(function(){
-      readURL(this);
+    }
+
   });
 });
 
-$(function() {
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-    $('.img_prev').attr('src', e.target.result);
-        }
-        reader.readAsDataURL(input.files[0]);
-        }
-    }
-    $(document).on('change', '.img_field', function(){
-      readURL(this);
-    });
-  });
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,23 @@ class User < ApplicationRecord
 
   has_one_attached :image
 
-  has_many :artcles, dependent: :destroy
+  #has_many :articles, dependent: :destroy
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates :username, presence: true, length: { maximum: 20 }
+  validates :profile, length: { maximum: 1000 }
+
+
+
+  def update_without_current_password(params, *options)
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
 end
