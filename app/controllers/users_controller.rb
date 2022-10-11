b66ba0a6ba1a2fdabf92c77b064ae7052847c73c
kitkat1900 <moco1900@gmail.com>
2022-10-12 01:22:40 +0900

    ユーザー画面の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,7 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+    @articles = @user.articles.status_public.order(created_at: :desc)
+    @unless_articles = Article.includes(:user).order('created_at DESC')
   end
 end
