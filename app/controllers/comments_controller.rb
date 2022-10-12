b4014a681cfcf94d78e6652c44d14973d34f5990
kitkat1900 <moco1900@gmail.com>
2022-10-13 01:54:23 +0900

    commentsコントローラーを編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to article_path(params[:article_id])
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, article_id: params[:article_id])
+  end
 end
