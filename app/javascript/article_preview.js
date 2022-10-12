142be960475fa28df4dc92b650a8cc992db46bb5
kitkat1900 <moco1900@gmail.com>
2022-10-13 01:35:39 +0900

    投稿記事編集画面の修正

--- app/javascript/article_preview.js
+++ app/javascript/article_preview.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
   const postForm = document.getElementById('new_post');
   // プレビューを表示するためのスペースを取得
   const previewList = document.getElementById('previews');
+  const articleList = document.getElementById('article-img');
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!postForm) return null;
   console.log("preview.jsが読み込まれました");
@@ -16,19 +17,32 @@ document.addEventListener('DOMContentLoaded', function(){
     if (alreadyPreview) {
       alreadyPreview.remove();
     };
+    const articleImage = document.querySelector('#article-img');
+    if (articleImage) {
+      articleImage.remove();
+    }
     const file = e.target.files[0];
-    console.log(file);
-    const blob = window.URL.createObjectURL(file);
-    // 画像を表示するためのdiv要素を生成
-    const previewWrapper = document.createElement('div');
-    previewWrapper.setAttribute('class', 'preview');
-    // 表示する画像を生成
-    const previewImage = document.createElement('img');
-    previewImage.setAttribute('class', 'article-image');
-    previewImage.setAttribute('src', blob);
+    if (file) {
+      const blob = window.URL.createObjectURL(file);
+      // 画像を表示するためのdiv要素を生成
+      const previewWrapper = document.createElement('div');
+      previewWrapper.setAttribute('class', 'preview');
+      // 表示する画像を生成
+      const previewImage = document.createElement('img');
+      previewImage.setAttribute('class', 'article-image');
+      previewImage.setAttribute('src', blob);
 
-    // 生成したHTMLの要素をブラウザに表示させる
-    previewWrapper.appendChild(previewImage);
-    previewList.appendChild(previewWrapper);
+      // 生成したHTMLの要素をブラウザに表示させる
+      previewWrapper.appendChild(previewImage);
+      previewList.appendChild(previewWrapper);
+    } else {
+      const articleWrapper = document.createElement('div');
+      articleWrapper.setAttribute('class', 'article');
+      const articleImage = document.createElement('img')
+      articleImage.setAttribute('class', 'article-image');
+      articleImage.setAttribute('src', articleList.src);
+      articleWrapper.appendChild(articleImage);
+      previewList.appendChild(articleWrapper);
+    }
   });
 });
